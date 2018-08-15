bda8069d09a0badbff59f216781fd237b7d0f9a9
Mariam <mariam@localhost.localdomain>
2018-08-15 17:40:35 +0300

    created database connect and added the app donate in the settings installed apps

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'donate',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,10 @@ WSGI_APPLICATION = 'bloodbank.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+         'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'connect',
+        'USER': 'mariam',
+    'PASSWORD':'bloodbank',
     }
 }
 
