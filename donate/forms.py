539d0042350c5349926f0fcd1fa27d0a67eac5b7
victor njuguna <victormoore254@gmail.com>
2018-08-20 11:42:03 +0300

    creates the donation model and modifies aspects of the bloodbank model

--- donate/forms.py
+++ donate/forms.py
@@ -1,4 +1,5 @@
 from django import forms
+from django.contrib.auth import forms as auth_forms
 from django.contrib.auth.forms import UserCreationForm
 from . models import BloodBank
 from django.contrib.auth import get_user_model
@@ -10,6 +11,31 @@ class DonorSignupForm(UserCreationForm):
         fields = ('first_name', 'last_name', 'username', 'phone_number', 'gender', 'blood_type')
 
 class BankSignupForm(forms.ModelForm):
+    error_messages = {
+        'password_mismatch': "The two password fields didn't match.",
+    }
+    password1 = forms.CharField(label = "Password", widget = forms.PasswordInput)
+    password2 = forms.CharField(label = "Password confirmation", widget = forms.PasswordInput,
+    help_text = "Enter the same password as above, for verification.")
+
+
     class Meta:
         model = BloodBank
-        fields = ('name', 'location', 'email', 'phone_number')
+        fields = ('username', 'location', 'email', 'phone_number')
+
+    def clean_password2(self):
+        password1 = self.cleaned_data.get("password1")
+        password2 = self.cleaned_data.get("password2")
+        if password1 and password2 and password1 != password2:
+            raise forms.ValidationError(
+                self.error_messages['password_mismatch'],
+                code = 'password_mismatch'
+            )
+        return password2
+
+    def save(self, commit = True):
+        user = super(BankSignupForm, self).save(commit = False)
+        user.set_password(self.cleaned_data["password1"])
+        if commit:
+            user.save()
+        return user
--- donate/models.py
+++ donate/models.py
@@ -50,8 +50,14 @@ def save_user_profile(sender, instance, **kwargs):
    instance.profile.save()
 
 class BloodBank(models.Model):
-   name = models.CharField(max_length = 130)
+   username = models.CharField(max_length = 130)
    location = models.CharField(max_length = 100)
    email = models.EmailField()
    phone_regex = RegexValidator(regex=r'^\+?1?\d{9,15}$', message="Phone number must be entered in the format: '+254123456789'. Up to 15 digits allowed.")
    phone_number = models.CharField(validators=[phone_regex], max_length=17, blank=True) # validators should be a list
+#    password = models.CharField(max_length=50)
+
+class Donation(models.Model):
+    location = models.CharField(max_length = 255)
+    starts_at = models.DateTimeField(auto_now_add = True)
+    ends_at = models.DateTimeField(auto_now = True)
