3e0bbc2f5dc787281d099a91247e661ac57421bb
kimani-njoroge <kimani064@gmail.com>
2018-08-20 17:25:30 +0300

    create bloodbank signup view

--- donate/views.py
+++ donate/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from donate.models import Profile
-from .forms import DonorSignupForm,BankSignupForm, DonationForm
+from .forms import DonorSignupForm, DonationForm
 from django.contrib.auth import get_user_model
 User = get_user_model()
 
@@ -9,29 +9,33 @@ User = get_user_model()
 def home(request):
     return render(request,'home.html')
 
-def banksignup(request):
-    if request.method == 'POST':
-        form = BankSignupForm(request.POST)
-        if form.is_valid():
-            user = form.save(commit = False)
-            user.is_active =True
-            user.save()
-            return redirect('/')
-    else:
-        form = BankSignupForm()
-    return render(request,'registration/banksignup.html', { "form":form })
-
 def donorsignup(request):
     if request.method == 'POST':
         form = DonorSignupForm(request.POST)
         if form.is_valid():
             user = form.save(commit = False)
             user.is_active =True
+            user.is_donor = True
+            user.is_bbank = False
             user.save()
             return redirect('/')
     else:
         form = DonorSignupForm()
     return render(request,'registration/donorsignup.html', { "form":form })
+
+def banksignup(request):
+    if request.method == 'POST':
+        form = DonorSignupForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit = False)
+            user.is_active =True
+            user.is_donor = False
+            user.is_bbank = True
+            user.save()
+            return redirect('/')
+    else:
+        form = DonorSignupForm()
+    return render(request,'registration/banksignup.html', { "form":form })
 
 
 def donation(request):
