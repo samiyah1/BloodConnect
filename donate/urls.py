cb8ca6af65af530d985668cc7a9310a3f5a8f795
Mariam <mariam@localhost.localdomain>
2018-08-20 14:29:32 +0300

    created all home url

--- donate/urls.py
+++ donate/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
+    url(r'^$', views.home, name='home'),
     url(r'^$', views.index, name='index'),
     url(r'signup/donor/',views.donorsignup, name = 'donor-signup'),
     url(r'signup/bank/',views.banksignup, name = 'bank-signup'),
