b95bdb758cc82f72526e4ccc291a50355284a156
victor njuguna <victormoore254@gmail.com>
2018-08-20 15:36:30 +0300

    resolve conflicts

--- donate/urls.py
+++ donate/urls.py
@@ -2,7 +2,6 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url(r'^$', views.donation, name='donation'),
     url(r'signup/donor/',views.donorsignup, name = 'donor-signup'),
     url(r'signup/bank/',views.banksignup, name = 'bank-signup'),
 ]
