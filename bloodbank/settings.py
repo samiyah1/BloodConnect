73d6ef1c4a0a0b59d4160a485aa12b0e5d8e2fe3
Mariam <mariam@localhost.localdomain>
2018-08-23 16:11:20 +0300

    git add user's name to mariam in settings

--- bloodbank/settings.py
+++ bloodbank/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'connect',
-        'USER': 'sam',
+        'USER': 'mariam',
         'PASSWORD':'  ',
     }
 }
